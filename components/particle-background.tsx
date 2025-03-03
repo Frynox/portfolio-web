3c50dc5556f1358e88afeb8040fff03289a67d6b
Frynox <neiljosuemontano@gmail.com>
2025-03-03 13:10:27 -0300

    fixing problem

--- components/particle-background.tsx
+++ components/particle-background.tsx
@@ -33,8 +33,8 @@ export function ParticleBackground() {
       speedY: number
 
       constructor() {
-        this.x = Math.random() * canvas.width
-        this.y = Math.random() * canvas.height
+        this.x = Math.random() * canvas!.width
+        this.y = Math.random() * canvas!.height
         this.size = Math.random() * 2 + 0.5
         this.speedX = Math.random() * 0.5 - 0.25
         this.speedY = Math.random() * 0.5 - 0.25
@@ -44,18 +44,18 @@ export function ParticleBackground() {
         this.x += this.speedX
         this.y += this.speedY
 
-        if (this.x > canvas.width) this.x = 0
-        else if (this.x < 0) this.x = canvas.width
+        if (this.x > canvas!.width) this.x = 0
+        else if (this.x < 0) this.x = canvas!.width
 
-        if (this.y > canvas.height) this.y = 0
-        else if (this.y < 0) this.y = canvas.height
+        if (this.y > canvas!.height) this.y = 0
+        else if (this.y < 0) this.y = canvas!.height
       }
 
       draw() {
-        ctx.fillStyle = "rgba(255, 255, 255, 0.5)"
-        ctx.beginPath()
-        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2)
-        ctx.fill()
+        ctx!.fillStyle = "rgba(255, 255, 255, 0.5)"
+        ctx!.beginPath()
+        ctx!.arc(this.x, this.y, this.size, 0, Math.PI * 2)
+        ctx!.fill()
       }
     }
 
@@ -74,19 +74,19 @@ export function ParticleBackground() {
 
           if (distance < 100) {
             const opacity = 1 - distance / 100
-            ctx.strokeStyle = `rgba(255, 255, 255, ${opacity * 0.2})`
-            ctx.lineWidth = 1
-            ctx.beginPath()
-            ctx.moveTo(particlesArray[a].x, particlesArray[a].y)
-            ctx.lineTo(particlesArray[b].x, particlesArray[b].y)
-            ctx.stroke()
+            ctx!.strokeStyle = `rgba(255, 255, 255, ${opacity * 0.2})`
+            ctx!.lineWidth = 1
+            ctx!.beginPath()
+            ctx!.moveTo(particlesArray[a].x, particlesArray[a].y)
+            ctx!.lineTo(particlesArray[b].x, particlesArray[b].y)
+            ctx!.stroke()
           }
         }
       }
     }
 
     function animate() {
-      ctx.clearRect(0, 0, canvas.width, canvas.height)
+      ctx!.clearRect(0, 0, canvas!.width, canvas!.height)
 
       for (let i = 0; i < particlesArray.length; i++) {
         particlesArray[i].update()
@@ -107,4 +107,3 @@ export function ParticleBackground() {
 
   return <canvas ref={canvasRef} className="fixed top-0 left-0 w-full h-full pointer-events-none z-0" />
 }
-
